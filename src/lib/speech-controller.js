5635833798cb74969cde1d91125d13086d499513
Guillaume Marty <edo999@gmail.com>
2016-10-05 16:40:43 +0100

    Define intent-parser as a dependency

--- src/lib/speech-controller.js
+++ src/lib/speech-controller.js
@@ -1,8 +1,8 @@
+import IntentParser from 'intent-parser';
 import EventDispatcher from './common/event-dispatcher';
 import WakeWordRecogniser from './wakeword/recogniser';
 import SpeechRecogniser from './speech/recogniser';
 import SpeechSynthesis from './speech/synthesis';
-import IntentParser from './intent-parser/index';
 
 const p = Object.freeze({
   // Properties
